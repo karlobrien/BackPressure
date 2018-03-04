0987c86cd36f3a5d8e08bc6acb74b2e8f19325ba
Karl O'Brien <karlobrien@gmail.com>
2018-03-04 13:05:33 +0000

    project setup

---
+++ src/DataFlowConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFlowConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
