ef0432c98c1fa278df3093ed12b873e66938551d
Karl O'Brien <karlobrien@gmail.com>
2018-03-05 22:12:15 +0000

    blocking collection and dataflow examples

--- src/DataFlowConsole/Program.cs
+++ src/DataFlowConsole/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace DataFlowConsole
 {
-    class Program
+    public class Program
     {
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += (sender, eArgs) => {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            Console.WriteLine("Kick off a blocking collection");
+
+            var prod = Task.Run(() => BlockExample.Producer());
+            var con = Task.Run(() => BlockExample.Consumer());
+
+            _quitEvent.WaitOne();
         }
+    }
+
+    public class BlockExample
+    {
+        private static BlockingCollection<int> data = new BlockingCollection<int>(boundedCapacity:5);
+        public static void Producer()
+        {
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+               data.Add(ctr);
+               Console.WriteLine($"Added ctr: {ctr}");
+            }
+        }
+
+        public static void Consumer()
+        {
+            foreach (var item in data.GetConsumingEnumerable())
+            {
+                Console.WriteLine($"Removed {item}");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class BothWays
+    {
+        public static void Produce(BufferBlock<int> queue, IEnumerable<int> values)
+        {
+            foreach (var value in values)
+            {
+                queue.Post(value);
+            }
+
+            queue.Complete();
+        }
+
+        public static async Task<IEnumerable<int>> Consume(BufferBlock<int> queue)
+        {
+            var ret = new List<int>();
+            while (await queue.OutputAvailableAsync())
+            {
+                ret.Add(await queue.ReceiveAsync());
+            }
+
+            return ret;
+        }
+
     }
 }
---
+++ test/DataFlow.Test/Simple.Unit.Test.cs
@@ -0,0 +1,32 @@
+using System;
+using DataFlowConsole;
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+using System.Linq;
+
+namespace SimpleUnitTest
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public async Task ConsumerReceivesCorrectValues()
+        {
+            // Define the mesh.
+            var queue = new BufferBlock<int>();
+
+            // Start the producer and consumer.
+            var values = Enumerable.Range(0, 10);
+            BothWays.Produce(queue, values);
+            var consumer = BothWays.Consume(queue);
+
+            // Wait for everything to complete.
+            await Task.WhenAll(consumer, queue.Completion);
+
+            // Ensure the consumer got what the producer sent (in the correct order).
+            var results = await consumer;
+            Assert.True(results.SequenceEqual(values));
+        }
+    }
+}
