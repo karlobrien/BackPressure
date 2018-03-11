81786f655e662a6a458c2502ef47980cd1af6687
Karl O'Brien <karlobrien@gmail.com>
2018-03-11 21:58:29 +0000

    blocking with wait and pulse

---
+++ src/DataFlowConsole/CustomBlockingQueue.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DataFlowConsole
+{
+    public class CustomBlockingQueue<TIn>
+    {
+        private readonly object _objForLock = new Object();
+        private Queue<TIn> _queue = new Queue<TIn>();
+        private readonly int _size;
+        private bool _shutdown = false;
+
+        public CustomBlockingQueue(int size)
+        {
+            _size = size;
+        }
+
+        public void Stop()
+        {
+            lock (_objForLock)
+            {
+                _shutdown = true;
+                Monitor.PulseAll(_objForLock);
+            }
+        }
+        public bool Enqueue(TIn item)
+        {
+            lock(_objForLock)
+            {
+                while (_queue.Count > _size && !_shutdown)
+                {
+                    Monitor.Wait(_objForLock);
+                }
+
+                if (_shutdown)
+                    return false;
+
+                _queue.Enqueue(item);
+                Monitor.PulseAll(_objForLock);
+            }
+
+            return true;
+        }
+
+        public bool Dequeue(out TIn item)
+        {
+            item = default(TIn);
+
+            lock(_objForLock)
+            {
+                while(_queue.Count == 0 && !_shutdown)
+                {
+                    Monitor.Wait(_objForLock);
+                }
+
+                if (!_queue.TryDequeue(out item))
+                    return false;
+
+                Monitor.PulseAll(_objForLock);
+            }
+
+            return true;
+        }
+    }
+}
--- src/DataFlowConsole/Program.cs
+++ src/DataFlowConsole/Program.cs
@@ -14,14 +14,86 @@ namespace DataFlowConsole
                 eArgs.Cancel = true;
             };
 
-            Console.WriteLine("Kick off a blocking collection");
-
-            var prod = Task.Run(() => BlockExample.Producer());
-            var con = Task.Run(() => BlockExample.Consumer());
+            CustomBlockQueue();
 
             _quitEvent.WaitOne();
 
             //Clean up the remaining items
+        }
+
+        public static void CustomBlockQueue()
+        {
+            var bq = new CustomBlockingQueue<int>(10);
+
+            Task producer = new Task(() =>
+            {
+                for(var i = 0; ; i++)
+                {
+                    if (!bq.Enqueue(i))
+                        break;
+                    Console.WriteLine($"{i} >>");
+                }
+                Console.WriteLine("Producer has completed......");
+            });
+            producer.Start();
+
+            Task consumer = new Task(() =>
+            {
+                    for (; ;)
+                    {
+                        Thread.Sleep(100);
+                        int x = 0;
+                        if ( !bq.Dequeue( out x ) )
+                            break;
+                        Console.WriteLine($"{0} < {x}");
+                    }
+                    Console.WriteLine($"Consumer {0} has completed......");
+            });
+            consumer.Start();
+
+            Task consumerTwo = new Task(() =>
+            {
+                for (; ;)
+                {
+                    Thread.Sleep(100);
+                    int x = 0;
+                    if ( !bq.Dequeue( out x ) )
+                        break;
+                    Console.WriteLine($"{1} < {x}");
+                }
+                Console.WriteLine($"Consumer {1} has completed......");
+            });
+            consumerTwo.Start();
+
+
+            Task consumerThree = new Task(() =>
+            {
+                for (; ;)
+                {
+                    Thread.Sleep(100);
+                    int x = 0;
+                    if ( !bq.Dequeue( out x ) )
+                        break;
+                    Console.WriteLine($"{2} < {x}");
+                }
+                Console.WriteLine($"Consumer {2} has completed......");
+            });
+            consumerThree.Start();
+
+            Thread.Sleep( 1000 );
+
+            Console.WriteLine( "Quitting" );
+
+            bq.Stop();
+
+        }
+
+        public static void DotNetBlockingCollection()
+        {
+            Console.WriteLine("Kick off a blocking collection");
+
+            var prod = Task.Run(() => BlockExample.Producer());
+            var con = Task.Run(() => BlockExample.Consumer());
         }
     }
 }
